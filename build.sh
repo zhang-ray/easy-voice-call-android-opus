04db76aa074615333997f98f6e323b142e8ca9b0
Ray Zhang <zhangruihuai@gmail.com>
2019-08-28 14:28:21 +0800

    update .travis.yml

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 tar -zxvf opus-1.1.2.tar.gz 
-wget https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip
-unzip android-ndk-r17c-linux-x86_64.zip
-./android-ndk-r17c/ndk-build APP_BUILD_SCRIPT=Android.mk NDK_PROJECT_PATH=build
+wget https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip > /dev/null
+unzip android-ndk-r17c-linux-x86_64.zip > /dev/null
+./android-ndk-r17c/ndk-build APP_BUILD_SCRIPT=Android.mk NDK_PROJECT_PATH=build > out.log
