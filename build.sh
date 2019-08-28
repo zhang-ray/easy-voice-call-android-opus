dd1c806f80359c373973d11e06759a687532e706
Ray Zhang <zhangruihuai@gmail.com>
2019-08-28 14:22:02 +0800

    + .travis.yml

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 tar -zxvf opus-1.1.2.tar.gz 
+wget https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip
+unzip android-ndk-r17c-linux-x86_64.zip
+./android-ndk-r17c/ndk-build APP_BUILD_SCRIPT=Android.mk NDK_PROJECT_PATH=build
